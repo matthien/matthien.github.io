b1ce5c7a103dcff4f2c6bb6570066cfc1e61febd
Matthew Thien <mthien819@gmail.com>
2020-05-13 17:38:50 -0400

    .

--- cisc 3610 pages/labs/lab4/main.js
+++ cisc 3610 pages/labs/lab4/main.js
@@ -24,11 +24,11 @@ var config = {
     function create () {
     
        var tree = this.add.image( 500, 300, 'tree');
-       var pear = this.add.image( 50, 450, 'pear').setScale(0.1);
-       var orange = this.add.image( 50, 150, 'orange').setScale(0.05);
-       var apple = this.add.image( 50, 250, 'apple').setScale(0.1);
-       var peach = this.add.image( 50, 350, 'peach').setScale(0.05);
-       var cherry = this.add.image( 50, 50, 'cherry').setScale(0.1);
+       var pear = this.add.image( 100, 450, 'pear').setScale(0.1);
+       var orange = this.add.image( 100, 150, 'orange').setScale(0.05);
+       var apple = this.add.image( 100, 250, 'apple').setScale(0.1);
+       var peach = this.add.image( 100, 350, 'peach').setScale(0.05);
+       var cherry = this.add.image( 100, 50, 'cherry').setScale(0.1);
        
       
        //tree.setInteractive();
