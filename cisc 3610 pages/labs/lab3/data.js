926d13013ef77a44a2c65e12452670f717812c79
Matthew Thien <mthien819@gmail.com>
2020-05-13 18:30:42 -0400

    .

--- cisc 3610 pages/labs/lab3/data.js
+++
@@ -1,5 +0,0 @@
-function inputChange() { 
-	var x = document.getElementById("title").onchange = function() { 
-		localStorage.setItem("title", );
-   }
-}
---
+++ cisc 3610 pages/labs/lab3/main.js
@@ -0,0 +1,44 @@
+var oldSave = { 
+	oldTitle: undefined,
+	oldText: undefined
+};
+
+	function storeTitleLocal(e) {
+		oldTitleLocal(e)
+		localStorage.setItem('title', e);
+	};
+	
+	function storeTextLocal(e) { 
+		oldTextLocal(e)
+		localStorage.setItem('text', e);
+	};
+	
+	function onLoad() { 
+		var element = document.getElementById('titleText');
+		var titleValue=localStorage.getItem("title");
+		element.value = titleValue;
+		
+		var textElement = document.getElementById('textarea');
+		var textValue = localStorage.getItem("text");
+		textElement.value = textValue;
+	};
+	
+	function oldTitleLocal(e) { 
+		oldSave.oldTitle = e;
+	}
+	
+	function oldTextLocal(e) { 
+		oldSave.oldText = e;
+	}
+	
+	function undoButton() { 
+		document.getElementById("titleText").value = oldSave.oldTitle;
+		document.getElementById("textarea").value = oldSave.oldText;
+	}
+	
+	function clearButton() { 
+		document.getElementById("titleText").value = " ";
+		document.getElementById("textarea").value = " ";
+	}
+	
+	document.addEventListener('DOMContentLoaded', onLoad);
