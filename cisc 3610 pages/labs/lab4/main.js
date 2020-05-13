8cb0ae1acd4135fcb6f7a990e06d5902e8b9da01
Matthew Thien <mthien819@gmail.com>
2020-05-13 17:12:07 -0400

    .

--- cisc 3610 pages/labs/lab4/main.js
+++ cisc 3610 pages/labs/lab4/main.js
@@ -1,29 +1,48 @@
 var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 800,
-    _parent: 'phaser-example',
-    backgroundColor: '#efefef',
-    scene: {
-        preload: preload,
-        create: create
+        type: Phaser.AUTO,
+        width: 600,
+        height: 600,
+        backgroundColor: '#efefef',
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    var game = new Phaser.Game(config);
+
+    function preload () {
+        this.load.image('pear', 'images/pear.png');
+        this.load.image('orange', 'images/orange.png');
+        this.load.image('apple', 'images/apple.png');
+        this.load.image('tree', 'images/tree.png');
+        this.load.image('cherry', 'images/cherry.png');
+        this.load.image('peach', 'images/peach.png');    
     }
-};
 
-var game = new Phaser.Game(config);
+    function create () {
+       var tree = this.add.image( 400, 300, 'tree');
+       var pear = this.add.image( 50, 450, 'pear');
+       var orange = this.add.image( 50, 150, 'orange');
+       var apple = this.add.image( 50, 250, 'apple');
+       var peach = this.add.image( 50, 350, 'peach');
+       var cherry = this.add.image( 50, 550, 'cherry');
+      
+       //tree.setInteractive();
+       pear.setInteractive();
+       orange.setInteractive();
+       apple.setInteractive();
+       peach.setInteractive();
+       cherry.setInteractive();
 
-function preload ()
-{
-    this.load.image('tree', 'images/pear.jpg');
-}
 
-function create ()
-{
-    var tree = this.add.image(500, 500, 'tree');
-
-    tree.setInteractive();
-
-    this.input.setDraggable(tree);
+       //this.input.setDraggable(tree);
+       this.input.setDraggable(pear);
+       this.input.setDraggable(orange);
+       this.input.setDraggable(apple);
+       this.input.setDraggable(peach);
+       this.input.setDraggable(cherry);
 
     this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
 
@@ -31,4 +50,9 @@ function create ()
         gameObject.y = dragY;
 
     });
-}
+    }
+
+    function update ()
+    {
+    }
+
