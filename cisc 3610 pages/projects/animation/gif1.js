24dab53352d03d434b982139f9f33501062e30cd
Matthew Thien <mthien819@gmail.com>
2020-05-14 14:43:09 -0400

    .

---
+++ cisc 3610 pages/projects/animation/gif1.js
@@ -0,0 +1,63 @@
+runGif={
+	src : "run.png",
+	img : undefined,
+	frame: 0,
+	offset: 0,
+	"frames": [
+		{
+			"filename": "tile000.png",
+			"frame": {
+					"x": 1,
+					"y": 1,
+					"w": 400,
+					"h": 240
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 400,
+				"h": 240
+			},
+			"sourceSize": {
+					"w": 400,
+					"h": 240
+			}	
+			
+			
+		},
+		{
+			"filename": "tile001.png",
+			"frame": {
+					"x": 403,
+					"y": 1,
+					"w": 400,
+					"h": 240
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 400,
+				"h": 240
+			},
+			"sourceSize": {
+					"w": 400,
+					"h": 240
+			}	
+		}
+	],
+	"meta": {
+		"app": "http://www.codeandweb.com/texturepacker",
+		"version": "1.0",
+		"image": "run.png",
+		"format": "RGBA8888",
+		"size": {
+			"w": 867,
+			"h": 798
+		},
+		"scale": "1"
+	}
+}
