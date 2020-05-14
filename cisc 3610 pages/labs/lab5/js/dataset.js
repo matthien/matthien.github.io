670f416ea0550c8074b2a20dafd2bb80f1ec9613
Matthew Thien <mthien819@gmail.com>
2020-05-14 13:40:54 -0400

    .

---
+++ cisc 3610 pages/labs/lab5/js/dataset.js
@@ -0,0 +1,25 @@
+Info = { 
+ "arr" : [
+  {
+    "Year": 2009,
+    "HomelessEstimate": 6016
+
+  },
+  {
+    "Year": 2010,
+    "HomelessEstimate": 8256
+
+  },
+  {
+    "Year": 2011,
+    "HomelessEstimate": 6669
+
+  },
+  {
+    "Year": 2012,
+    "HomelessEstimate": 8152
+
+  }
+]
+};
+
