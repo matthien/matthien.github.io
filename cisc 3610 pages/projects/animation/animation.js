696e093e04250b21851e90e2873c84e408a347a0
Matthew Thien <mthien819@gmail.com>
2020-05-14 15:13:19 -0400

    .

--- cisc 3610 pages/projects/animation/animation.js
+++ cisc 3610 pages/projects/animation/animation.js
@@ -62,7 +62,69 @@ Scene.draw = function () {
 	// At the end of the sprite sheet,Clear the canvas and call second gif
 	if(Scene.sprite.frame==Scene.sprite.frames.length){
             Scene.clearCanvas();
-            Scene2.start();
+            Scene1.start();
         }
 };
 
+var Scene1 = {
+    canvas : undefined,
+    canvasContext : undefined,
+    sprite: undefined
+};
+
+Scene1.start = function () {
+    // Get the canvas and it's context.
+    Scene1.canvas = document.getElementById("myCanvas");
+    Scene1.canvasContext = Scene1.canvas.getContext("2d");
+	
+    // Seup the sold out to be displayed.
+    Scene1.sprite = fireGif;
+	
+    // Attach the image to be used for the sprite.
+    Scene1.sprite.img = new Image();
+    Scene1.sprite.img.src = Scene1.sprite.src;
+    
+    Scene1.sprite.frame=0;
+    
+    // Wait till the sold out image is loaded before starting the animation.
+    Scene1.sprite.img.onload = function() {		
+    	Scene1.mainLoop();
+    };
+};
+
+Scene1.mainLoop = function() {
+    //Scene1.clearCanvas();
+    //Scene1.update();
+    Scene1.draw();
+    
+	
+	// Animate at 10 frames a second.
+    window.setTimeout(Scene1.mainLoop, 1000 / 10);
+};
+
+Scene1.clearCanvas = function () {
+    Scene1.canvasContext.fillStyle = "white";
+    Scene1.canvasContext.fillRect(0, 0, Scene1.canvas.width, Scene1.canvas.height);
+};
+
+Scene1.update = function () {
+	// Set the canvas width to be that of the display Window. Which helps if you resize the window.
+  	Scene1.canvas.width = window.innerWidth;
+};
+
+Scene1.draw = function () {
+	Scene1.canvasContext.drawImage(Scene1.sprite.img,Scene1.sprite.frames[Scene1.sprite.frame].frame.x,Scene1.sprite.frames[Scene1.sprite.frame].frame.y,Scene1.sprite.frames[Scene1.sprite.frame].frame.w,Scene1.sprite.frames[Scene1.sprite.frame].frame.h,0,0,Scene1.sprite.frames[Scene1.sprite.frame].frame.w,Scene1.sprite.frames[Scene1.sprite.frame].frame.h);
+	
+    Scene1.canvasContext.font = "20px Arial";
+	Scene1.canvasContext.fillStyle = "black";
+	Scene1.canvasContext.fillText("IM COMING!!", 350, 250);
+	   
+	// Advance to the next frame.
+	Scene1.sprite.frame++;
+
+	// At the end of the sprite sheet, restart the first gif
+	if(Scene1.sprite.frame==Scene1.sprite.frames.length){
+            Scene1.clearCanvas();
+            Scene.start();
+        }
+};
---
+++ cisc 3610 pages/projects/animation/gif2.js
@@ -0,0 +1,230 @@
+fireGif={
+	src : "images/fire/fire.png",
+	img : undefined,
+	frame: 0,
+	offset: 0,
+	"frames": [
+		{
+			"filename": "tile000.png",
+			"frame": {
+					"x": 1,
+					"y": 1,
+					"w": 500,
+					"h": 280
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 500,
+				"h": 280
+			},
+			"sourceSize": {
+					"w": 500,
+					"h": 280
+			}	
+		},
+		{
+			"filename": "tile001.png",
+			"frame": {
+					"x": 501,
+					"y": 1,
+					"w": 500,
+					"h": 280
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 500,
+				"h": 280
+			},
+			"sourceSize": {
+					"w": 500,
+					"h": 280
+			}	
+		},
+		{
+			"filename": "tile002.png",
+			"frame": {
+					"x": 1001,
+					"y": 1,
+					"w": 500,
+					"h": 280
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 500,
+				"h": 280
+			},
+			"sourceSize": {
+					"w": 500,
+					"h": 280
+			}	
+		},
+		{
+			"filename": "tile003.png",
+			"frame": {
+					"x": 1501,
+					"y": 1,
+					"w": 500,
+					"h": 280
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 500,
+				"h": 280
+			},
+			"sourceSize": {
+					"w": 500,
+					"h": 280
+			}	
+		},
+		{
+			"filename": "tile004.png",
+			"frame": {
+					"x": 2001,
+					"y": 1,
+					"w": 500,
+					"h": 280
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 500,
+				"h": 280
+			},
+			"sourceSize": {
+					"w": 500,
+					"h": 290
+			}	
+		},
+		{
+			"filename": "tile005.png",
+			"frame": {
+					"x": 1,
+					"y": 281,
+					"w": 500,
+					"h": 280
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 500,
+				"h": 280
+			},
+			"sourceSize": {
+					"w": 500,
+					"h": 290
+			}	
+		},
+		{
+			"filename": "tile006.png",
+			"frame": {
+					"x": 501,
+					"y": 281,
+					"w": 500,
+					"h": 280
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 500,
+				"h": 280
+			},
+			"sourceSize": {
+					"w": 500,
+					"h": 290
+			}	
+		},
+		{
+			"filename": "tile007.png",
+			"frame": {
+					"x": 1001,
+					"y": 281,
+					"w": 500,
+					"h": 280
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 500,
+				"h": 280
+			},
+			"sourceSize": {
+					"w": 500,
+					"h": 290
+			}	
+		},
+		{
+			"filename": "tile008.png",
+			"frame": {
+					"x": 1501,
+					"y": 281,
+					"w": 500,
+					"h": 280
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 500,
+				"h": 280
+			},
+			"sourceSize": {
+					"w": 500,
+					"h": 290
+			}	
+		},
+		{
+			"filename": "tile009.png",
+			"frame": {
+					"x": 2001,
+					"y": 281,
+					"w": 500,
+					"h": 280
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": { 
+				"x": 0,
+				"y": 0,
+				"w": 500,
+				"h": 280
+			},
+			"sourceSize": {
+					"w": 500,
+					"h": 290
+			}	
+		}
+	],
+	"meta": {
+		"app": "http://www.codeandweb.com/texturepacker",
+		"version": "1.0",
+		"image": "run.png",
+		"format": "RGBA8888",
+		"size": {
+			"w": 867,
+			"h": 798
+		},
+		"scale": "1"
+	}
+}
+		
