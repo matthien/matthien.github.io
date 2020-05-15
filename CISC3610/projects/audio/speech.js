47b83c4b02979ad7f273b32253a24956c004e1b3
Matthew Thien <mthien819@gmail.com>
2020-05-15 15:15:29 -0400

    .

--- CISC3610/projects/audio/speech.js
+++ CISC3610/projects/audio/speech.js
@@ -12,9 +12,9 @@ ctx.arc(300, 400, 50, 0, 2 * Math.PI);
 ctx.stroke();
 
 var commands = {
-	/*'color *color': changeColor,
-	'background *color': bgColor,
-	'size *num': changeSize,*/
+	'color *color': changeColor,
+	'background *color': backgroundColor,
+	'size *num': changeSize,
 	'help': function() {
     	var msg = new SpeechSynthesisUtterance();
     	msg.text = "Say color, followed by a color, to set the circle color. \n\
@@ -41,4 +41,51 @@ function startButton(){
     	x.value = "Speak";
   	}
   	//annyang.start();  
+};
+
+function changeColor(word){
+	color = word;
+    clear();
+};
+
+function backgroundColor(word){
+	bgColor = word;
+	clear();
+};
+
+function changeSize(size){
+   
+    if(size < 1){
+            var msg = new SpeechSynthesisUtterance();
+            msg.text =  "Size too small, the minimize size is 1";
+            synth.speak(msg);
+    }
+    if(size > 300){
+            var msg = new SpeechSynthesisUtterance();
+            msg.text =  "Size too big, the size limit is 300";
+            synth.speak(msg);
+    }
+    else{
+        clear();
+    }
+};
+
+function clear(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, ctx.width, ctx.height);
+    update();
+};
+
+function update(){
+    ctx.beginPath();
+    ctx.fillStyle = bgColor;
+    ctx.rect(0, 0, ctx.width, ctx.height);
+    ctx.fill();
+    ctx.stroke();
+    
+    ctx.beginPath();
+    ctx.fillstyle = color;
+    ctx.arc(400, 300, size, 0, 2* Math.PI); 
+    ctx.fill();
+    ctx.stroke();
 }
